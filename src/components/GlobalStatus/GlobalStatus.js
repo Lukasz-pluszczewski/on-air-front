cf27d5cb6eb91cb55571a565f7ef2baa710301c5
Łukasz Pluszczewski <lukasz.pluszczewski@gmail.com>
2019-10-02 11:02:59 +0200

    Added URL on global page

--- src/components/GlobalStatus/GlobalStatus.js
+++ src/components/GlobalStatus/GlobalStatus.js
@@ -2,11 +2,12 @@ import React from 'react';
 import classnames from 'classnames';
 import onAirImage from '../../assets/on-air.jpg';
 
+import config from '../../config';
 import { UPDATE_EVENT_NAME } from '../../constants/statusEvent';
 import { useSocketValue, useSocket } from '../../hooks/useSocketIo';
+import Loader from "../Loader/Loader";
 
 import './GlobalStatus.scss';
-import Loader from "../Loader/Loader";
 
 const GlobalStatus = () => {
   const [socket, connected] = useSocket();
@@ -20,8 +21,11 @@ const GlobalStatus = () => {
     );
   }
 
+  const url = config.apiUrl.split(':')[0];
+
   return (
     <div className="GlobalStatus__container">
+      <div className="GlobalStatus__address">{url}</div>
       <img className={classnames('GlobalStatus__image', { 'GlobalStatus__image--active': status === 'on-air' })} src={onAirImage} />
     </div>
   );
