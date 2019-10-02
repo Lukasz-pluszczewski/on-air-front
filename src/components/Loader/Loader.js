f13816a84c2475c05e9df2d5ab00fc0f05f1fd5c
Łukasz Pluszczewski <lukasz.pluszczewski@gmail.com>
2019-10-02 11:03:52 +0200

    Changed loader color

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -2,7 +2,7 @@ import React  from 'react';
 import { Spinner } from "react-bootstrap";
 
 const Loader = () => (
-  <Spinner size="lg" animation="grow" role="status">
+  <Spinner size="lg" animation="grow" variant="light" role="status">
     <span className="sr-only">Loading...</span>
   </Spinner>
 );
